9d881aecce21aeb4682c24020d27b86ff2fef245
Tomrrowthing <1443964613@qq.com>
2017-07-08 12:11:45 +0800

    测试idea的git功能

---
+++ src/com/dabai/helloworld/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.dabai.helloworld;
+
+/**
+ * Created by Administrator on 2017/7/8.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
